dea3a35018361d6c7d5668fe01828498e5f41b84
fowrest <105675636+fowrest@users.noreply.github.com>
2022-05-21 11:48:35 +0200

    Fixed build not working and fixed export of types

--- react-declarative-hook-form/src/index.ts
+++ react-declarative-hook-form/src/index.ts
@@ -1,3 +1,7 @@
 import DeclarativeForm from './DeclarativeForm';
+import { InputRepository } from './inputRepository/InputRepository';
+import { Schema, SpecialType } from './Schema';
 
+export { SpecialType, InputRepository };
+export type { Schema };
 export default DeclarativeForm;
